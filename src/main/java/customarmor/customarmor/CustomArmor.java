34651c52598d09259c5e3528ad925b4816df777f
De1v <dejvklima@gmail.com>
2022-08-23 09:32:59 +0200

    Added Source Code

---
+++ src/main/java/customarmor/customarmor/CustomArmor.java
@@ -0,0 +1,16 @@
+package customarmor.customarmor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomArmor extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
